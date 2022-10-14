d9260d84e4272d2d20145b52590df94a7f8a2e5d
Yuvraj Singh <syuvraj1900@gmail.com>
2022-10-14 14:23:34 -0400

    final commit

--- blog-service.js
+++ blog-service.js
@@ -8,11 +8,14 @@ module.exports = {
     initialize,
     getAllPosts,
     getPublishedPosts,
-    getCategories
+    getCategories,
+    addPost,
+    getPostsByCategory,
+    getPostsByMinDate,
+    getPostById
 }
 
 function initialize () {
-    
     return new Promise((resolve, reject) => {
         let message = "";
         fs.readFile(path.join(__dirname, "data/posts.json"), 'utf8', (err, data) => {
@@ -21,7 +24,6 @@ function initialize () {
             };
             posts = JSON.parse(data);
         });
-
         fs.readFile(path.join(__dirname, "data/categories.json"), 'utf8', (err, data) => {
             if (err) {
                 message = "Error occurred while loading Categories data";
@@ -56,5 +58,52 @@ function getCategories() {
     return new Promise((resolve, reject) => {
         if(categories.length) resolve(categories)
         else reject("No results returned");
+    })
+}
+
+// Add post function
+function addPost(postData){
+    return new Promise((resolve, reject) => {
+        if(postData.published === undefined) {
+            postData.published = false;
+        } else postData.published = true;
+
+        postData.id = posts.length + 1;
+
+        posts.push(postData);
+
+        resolve(postData);
+    })
+}
+
+
+
+function getPostsByCategory (category){
+    return new Promise((resolve, reject) => {
+        const categoryPosts = posts.filter((post) => {
+            return post.category == category;
+        })
+
+        categoryPosts.length > 0 ? resolve(categoryPosts) : reject("no results returned");
+    })
+}
+
+function getPostsByMinDate (minDateStr){
+    return new Promise((resolve, reject) => {
+        const minDatePosts = posts.filter((post) => {
+            return new Date(post.postDate) >= new Date(minDateStr);
+        })
+
+        minDatePosts.length > 0 ? resolve(minDatePosts) : reject("no results returned");
+    })
+}
+
+function getPostById (id){
+    return new Promise((resolve, reject) => {
+        const idPosts = posts.filter((post) => {
+            return post.id == id;
+        })
+
+        idPosts.length > 0 ? resolve(idPosts) : reject("no results returned");
     })
 }
