982026c9729b7569f39da9794296dac84b240104
Yuvraj Singh <syuvraj1900@gmail.com>
2022-12-01 14:28:54 -0500

    AS6

--- auth-service.js
+++ auth-service.js
@@ -20,7 +20,7 @@ let User; // to be defined on new connection (see initialize)
 
 module.exports.initialize = function () {
     return new Promise(function (resolve, reject) {
-        let db = mongoose.createConnection("mongodb+srv://manreet:manreet@senecamanreet.wwafflw.mongodb.net/?retryWrites=true&w=majority");
+        let db = mongoose.createConnection("mongodb+srv://yuvraj:yuvraj@senecaweb.uqew7qu.mongodb.net/?retryWrites=true&w=majority");
 
         db.on('error', (err)=>{
             reject(err); // reject the promise with the provided error
