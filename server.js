1338e76dd9409fbf886f576d03b9b73ec5629f44
Yuvraj Singh <syuvraj1900@gmail.com>
2022-12-01 15:42:58 -0500

    AS6

--- server.js
+++ server.js
@@ -72,7 +72,7 @@
   
   app.use(clientSessions({
       cookieName: "session", // this is the object name that will be added to 'req'
-      secret: "FUCKOFF", // this should be a long un-guessable string.
+      secret: "something kept hidden or unexplained", // this should be a long un-guessable string.
       duration: 2 * 60 * 1000, // duration of the session in milliseconds (2 minutes)
       activeDuration: 1000 * 60 // the session will be extended by this many ms each request (1 minute)
   }));
